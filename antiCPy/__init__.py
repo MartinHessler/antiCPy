4833503177e337551411636e2a9d406b23e2d548
Martin Heßler <m_hess23@uni-muenster.de>
2022-02-11 15:18:24 +0100

    Update to __version__ = '0.0.2' and prepare release with DOI for the corresponding publication.

--- antiCPy/__init__.py
+++ antiCPy/__init__.py
@@ -7,7 +7,7 @@ if sys.version_info < (3, 6, 0):
         'The installed Python version reached its end-of-life. Please upgrade to a newer Python version for receiving '
         'further antiCPy updates.', Warning)
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 __author__ = 'Martin Heßler'
 
