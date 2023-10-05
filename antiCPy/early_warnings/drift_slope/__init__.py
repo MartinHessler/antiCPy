eeda7f4b69fa48ee0b000484d1834939bb9b1642
MartinHessler <m_hess23@uni-muenster.de>
2023-10-05 11:48:20 +0200

    Import helper function.

--- antiCPy/early_warnings/drift_slope/__init__.py
+++ antiCPy/early_warnings/drift_slope/__init__.py
@@ -2,5 +2,6 @@ from .langevin_estimation import LangevinEstimation
 from .binning_langevin_estimation import  BinningLangevinEstimation
 from .non_markov_estimation import  NonMarkovEstimation
 from .rocket_fast_resilience_estimation import  RocketFastResilienceEstimation
+from .summary_statistics_helper import _summary_statistics_helper
 
 __all__ = ['LangevinEstimation', 'BinningLangevinEstimation', 'NonMarkovEstimation', 'RocketFastResilienceEstimation']
