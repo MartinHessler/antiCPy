9a025588dac9a6e5636d2da39371748c61b8b83c
MartinHessler <m_hess23@uni-muenster.de>
2023-10-07 10:11:36 +0200

    Include helpers in init.

--- antiCPy/trend_extrapolation/__init__.py
+++ antiCPy/trend_extrapolation/__init__.py
@@ -1,4 +1,5 @@
 from .cp_segment_fit import CPSegmentFit
 from .batched_cp_segment_fit import BatchedCPSegmentFit
+from .batched_configs_helper import *
 
 __all__ = ['CPSegmentFit', 'BatchedCPSegmentFit']
